b92fc24fa82fb2721e2c555135c864ee18dadffd
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2024-11-19 17:45:10 -0300

    ajustes de funcoes inuteis

--- source.py
+++ source.py
@@ -17,9 +17,9 @@ def lerPasta():
         return arquivosCompativeis
     else:
         print("A pasta 'Imagens' não existe.")
-def calcularAngulo(Bordas):
-    x1,y1= cv2.boundingRect(Bordas[0])[:2]
-    x2,y2= cv2.boundingRect(Bordas[1])[:2]
+def calcularAngulo(Cantos):
+    x1,y1= cv2.boundingRect(Cantos[0])[:2]
+    x2,y2= cv2.boundingRect(Cantos[1])[:2]
     dx = x1-x2
     dy = y1-y2
     rad= math.atan2(dx,dy)
@@ -59,45 +59,26 @@ def redimensionarImagem(img):
 def cortarGabarito(img_path):
     img = cv2.imread(img_path)
     img = redimensionarImagem(img)
-    img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    # Inicializar respostas com valores em branco
-    respostas = {i: '' for i in range(1, 51)}
-    alternativas = ['A', 'B', 'C', 'D', 'E']
-
-    # Binarizar a imagem para detectar contornos
+    img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_cinza_tresh = cv2.threshold(img_cinza, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    
     cantos = [None, None]
 
-    # Encontrar contornos
     contornos, _ = cv2.findContours(img_cinza_tresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     area_minima, area_maxima = 50, 200
     contornos_filtrados = [cnt for cnt in contornos if area_minima < cv2.contourArea(cnt) < area_maxima]
 
-    # Definir variáveis para limites de detecção
     max_y, max_x = 0, 0
     min_y, min_x = float('inf'), float('inf')
 
-    def dividirEmSubRegioes(roi):
-        altura, largura = roi.shape
-        altura_da_sub_regiao = altura // 2
-        largura_da_sub_regiao = largura // 2
-        return [
-            roi[:altura_da_sub_regiao, :largura_da_sub_regiao],
-            roi[:altura_da_sub_regiao, largura_da_sub_regiao:],
-            roi[altura_da_sub_regiao:, :largura_da_sub_regiao],
-            roi[altura_da_sub_regiao:, largura_da_sub_regiao:]
-        ]
-
-    # Encontrar cantos da imagem de marcação
     for cnt in contornos_filtrados:
         x, y, largura, altura = cv2.boundingRect(cnt)
         roi = img_cinza_tresh[y:y+altura, x:x+largura]
-        sub_regioes = dividirEmSubRegioes(roi)
 
-        proporcoes = [cv2.countNonZero(sub_reg) / (sub_reg.shape[0] * sub_reg.shape[1]) for sub_reg in sub_regioes]
+        proporcao = cv2.countNonZero(roi) / (roi.shape[0] * roi.shape[1])
 
-        if np.max(proporcoes) > 0.7:
+        if proporcao > 0.7:
             if y < min_y and x < min_x:
                 cantos[0] = cnt
             elif y < min_y and x < max_x:
@@ -114,7 +95,6 @@ def cortarGabarito(img_path):
     if min_x < max_x and min_y < max_y:
         img = img[min_y+20:min_y+480, min_x+20:min_x+580]
 
-    # Processar a imagem rotacionada e cortada
     img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_cinza_tresh = cv2.threshold(img_cinza, 127, 255, cv2.THRESH_BINARY_INV)[1]
     contornos, _ = cv2.findContours(img_cinza_tresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -122,21 +102,22 @@ def cortarGabarito(img_path):
     contornos_filtrados = [cnt for cnt in contornos if area_minima < cv2.contourArea(cnt) < area_maxima]
     tamanho_x, tamanho_y = (round(img.shape[1] / 15), round(img.shape[0] / 4))
 
+    respostas = {i: '' for i in range(1, 51)}
+    alternativas = ['A', 'B', 'C', 'D', 'E']
+
     for cnt in contornos_filtrados:
         x, y, largura, altura = cv2.boundingRect(cnt)
         roi = img_cinza_tresh[y:y+altura, x:x+largura]
-        sub_regioes = dividirEmSubRegioes(roi)
 
-        proporcoes = [cv2.countNonZero(sub_reg) / (sub_reg.shape[0] * sub_reg.shape[1]) for sub_reg in sub_regioes]
+        proporcao = cv2.countNonZero(roi) / (roi.shape[0] * roi.shape[1])
 
-        if np.max(proporcoes) > 0.3:
+        if proporcao > 0.3:
             cv2.rectangle(img, (x, y), (x + largura, y + altura), (255, 255, 0), thickness=2)
             questao = 1 + (math.floor(x / tamanho_x)) + (math.floor(y / tamanho_y) * 15)
             alternativa = int((y - (tamanho_y * (math.floor(y / tamanho_y)))) / ((tamanho_y - 30) / 5))
 
             cv2.putText(img, f'{alternativas[alternativa - 1]}', (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, .5, (255, 0, 0), thickness=1)
             
-            # Atualizar a resposta da questão com a alternativa marcada
             if respostas[questao] == '':
                 respostas[questao] = alternativas[alternativa - 1]
             else:
@@ -146,6 +127,7 @@ def cortarGabarito(img_path):
         if respostas[questao] == '':
             respostas[questao] = "Vazio"
     return img, respostas
+
 
 def mostrarImagem(img, Name='imagem'):
     plt.imshow(img)
