9afe54b7df51bce5e377499df843a18d925e94e5
Eduardo Rodrigues <edu.rodrigues.ap@gmail.com>
2024-11-19 17:47:01 -0300

    .

--- main.py
+++ main.py
@@ -16,3 +16,4 @@ with open(nome_do_arquivo, "w") as arquivo:
         acertos = source.compararRespostas(respostas)
 
         arquivo.write(f'{imagem} - Acertos: {acertos}/50 - Respostas: {entrada}\n')
+print(f'Leitura concluida! Verifique o resultado no arquivo {nome_do_arquivo}')
